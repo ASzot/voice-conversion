75324a76c2419f3a267ca0f0cc1f2d63b0eb2ac9
Andrew Szot <me@andrewszot.com>
2017-10-16 18:30:07 -0700

    Added progress

--- convert_to_stft.py
+++ convert_to_stft.py
@@ -3,12 +3,12 @@ import pandas as pd
 import numpy as np
 import os
 import scipy.misc
+import util
 
 BASE_DATA_PATH = "/hdd/cs599/VCTK-Corpus/"
 SPEAKER_INFO_PATH = BASE_DATA_PATH + "speaker-info.txt"
 AUDIO_DATA_PATH = BASE_DATA_PATH + "wav48/"
-N_FFT = 2048
-CUTOFF_LEN = 125
+CUTOFF_LEN = 258
 SPECTRO_SAVE_PATH = "/hdd/cs599/spectro/"
 
 def parse_speaker_info(speaker_info_path):
@@ -35,15 +35,23 @@ i = 0
 for speaker_id, gender in speaker_info:
     print('%.2f%%' % ((float(i) / float(len(speaker_info))) * 100.))
     audio_dir = AUDIO_DATA_PATH + "p" + speaker_id + '/'
+    print('Loading for speaker %s' % speaker_id)
     for audio_file in os.listdir(audio_dir):
         x, fs = librosa.load(audio_dir + audio_file)
-        S = librosa.stft(x, N_FFT)
-        S = np.log1p(np.abs(S[:,:430]))
+        S = util.specgram(x)
 
         if S.shape[1] < CUTOFF_LEN:
             continue
 
-        cut_audio = S[:, :CUTOFF_LEN].T
+        cut_audio = S[:, :CUTOFF_LEN, :]
+
+        padded = np.zeros((258, CUTOFF_LEN, 3))
+        padded[:257, :, :2] = cut_audio
+
+        #audio = util.ispecgram(padded[:257, :, :2])
+        #librosa.output.write_wav('/hdd/cs599/output/test3.wav', x, fs)
+        #raise ValueError()
+
         if gender == "M":
             folder = "male/"
         elif gender == "F":
@@ -53,6 +61,7 @@ for speaker_id, gender in speaker_info:
 
         audio_file_name = audio_file.split('.')[0]
 
-        scipy.misc.imsave(SPECTRO_SAVE_PATH + folder + audio_file_name + ".jpg", cut_audio)
+        scipy.misc.imsave(SPECTRO_SAVE_PATH + folder + audio_file_name +
+                ".jpg", padded)
     i += 1
 
