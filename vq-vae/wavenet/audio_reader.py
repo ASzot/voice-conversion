ea5b93fa0c315540f2065dbd1092c3eccc061202
Sriram Somasundaram <srirams32@gmail.com>
2017-11-21 15:59:57 -0800

    removing redund mulaw

--- vq-vae/wavenet/audio_reader.py
+++ vq-vae/wavenet/audio_reader.py
@@ -83,21 +83,6 @@ def not_all_have_id(files):
             return True
     return False
 
-def mu_law(x, mu=255, int8=False):
-    """A TF implementation of Mu-Law encoding.
-  Args:
-    x: The audio samples to encode.
-    mu: The Mu to use in our Mu-Law.
-    int8: Use int8 encoding.
-  Returns:
-    out: The Mu-Law encoded int8 data.
-  """
-    out = tf.sign(x) * tf.log(1 + mu * tf.abs(x)) / np.log(1 + mu)
-    out = tf.floor(out * 128)
-    if int8:
-        out = tf.cast(out, tf.int8)
-    return out
-
 class AudioReader(object):
     '''Generic background audio reader that preprocesses audio files
     and enqueues them into a TensorFlow queue.'''
